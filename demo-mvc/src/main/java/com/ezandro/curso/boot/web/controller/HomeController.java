455d14f297504dea1e25b6fbeaffea07055e5eb2
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2020-08-30 21:15:01 -0300

    Cadastro de funcionários utilizando Spring MVC

---
+++ demo-mvc/src/main/java/com/ezandro/curso/boot/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ezandro.curso.boot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping(path = "/")
+	public String home() {
+		return "/home";
+	}
+}
