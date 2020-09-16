16485cc5b125df90c9360a3276690d800c6b757e
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2020-09-16 01:51:16 -0300

    Cadastro de funcionários utilizando Spring MVC

--- demo-mvc/src/main/java/com/ezandro/curso/boot/web/controller/FuncionarioController.java
+++ demo-mvc/src/main/java/com/ezandro/curso/boot/web/controller/FuncionarioController.java
@@ -1,20 +1,78 @@
 package com.ezandro.curso.boot.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.ezandro.curso.boot.domain.Cargo;
+import com.ezandro.curso.boot.domain.Funcionario;
+import com.ezandro.curso.boot.domain.UF;
+import com.ezandro.curso.boot.service.CargoService;
+import com.ezandro.curso.boot.service.FuncionarioService;
 
 @Controller
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 	
+	@Autowired
+	private FuncionarioService funcionarioService;
+	
+	@Autowired
+	private CargoService cargoService;
+	
 	@GetMapping(path = "/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Funcionario funcionario) {
 		return "/funcionario/cadastro";
 	}
 	
 	@GetMapping(path = "/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("funcionarios", funcionarioService.buscarTodos());
 		return "/funcionario/lista";
+	}
+	
+	@PostMapping(path = "/salvar")
+	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.salvar(funcionario);
+		attr.addFlashAttribute("success", "Funcionário cadastrado com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}
+	
+	@GetMapping(path = "/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("funcionario", funcionarioService.buscarPorId(id));
+		return "funcionario/cadastro";
+	}
+	
+	@PostMapping(path = "/editar")
+	public String editar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.editar(funcionario);
+		attr.addFlashAttribute("success", "Funcionário editado com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}
+	
+	@GetMapping(path = "/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+		funcionarioService.excluir(id);
+		attr.addFlashAttribute("success", "Funcionário removido com sucesso.");
+		return "redirect:/funcionarios/listar";
+	}
+	
+	@ModelAttribute("cargos")
+	public List<Cargo> getCargos() {
+		return cargoService.buscarTodos();
+	}
+	
+	@ModelAttribute("ufs")
+	public UF[] getUFs() {
+		return UF.values();
 	}
 }
