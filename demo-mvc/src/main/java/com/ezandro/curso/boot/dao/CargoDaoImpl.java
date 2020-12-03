bb250e2d2f6ca5ed05cedbe01febf3711b47a085
Ezandro Bueno <57026708+ezbueno@users.noreply.github.com>
2020-12-03 00:40:05 -0300

    Implementado o cálculo do total de páginas

--- demo-mvc/src/main/java/com/ezandro/curso/boot/dao/CargoDaoImpl.java
+++ demo-mvc/src/main/java/com/ezandro/curso/boot/dao/CargoDaoImpl.java
@@ -1,9 +1,34 @@
 package com.ezandro.curso.boot.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.ezandro.curso.boot.domain.Cargo;
+import com.ezandro.curso.boot.util.PaginacaoUtil;
 
 @Repository
 public class CargoDaoImpl extends AbstractDao<Cargo, Long> implements CargoDao {
+	
+	public PaginacaoUtil<Cargo> buscaPaginada(int pagina) {
+		int tamanho = 5;
+		int inicio = (pagina - 1) * tamanho;
+		
+		List<Cargo> cargos = getEntityManager()
+				.createQuery("SELECT c FROM Cargo ORDER BY c.nome ASC", Cargo.class)
+				.setFirstResult(inicio)
+				.setMaxResults(tamanho)
+				.getResultList();
+		
+		long totalRegistros = count();
+		long totalPaginas = (totalRegistros + (tamanho - 1));
+		
+		return new PaginacaoUtil<>(tamanho, pagina, totalPaginas, cargos);
+	}
+	
+	public long count() {
+		return getEntityManager()
+				.createQuery("SELECT COUNT(*) FROM Cargo", long.class)
+				.getSingleResult();			
+	}
 }
